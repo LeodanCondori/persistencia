42c51677aef91e553e425443679d68ffcd6c990e
LeodanCondori <leo7condori7@gmail.com>
2024-04-16 16:12:01 -0500

    first commit

---
+++ src/main/java/com/codigo/persistencia/dao/ProductoRepository.java
@@ -0,0 +1,6 @@
+package com.codigo.persistencia.dao;
+
+import com.codigo.persistencia.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+}
